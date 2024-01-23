6eab1f052a8959c1055472cf715f4637f5ff0c13
FunnyFatMan1 <92633939+FunnyFatMan1@users.noreply.github.com>
2024-01-23 19:52:28 +0100

    stworzenie klasy budzet

---
+++ Budzet.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudzetDomowyProjekt
+{
+    public class Budzet
+    {
+        public int Id { get; set; }
+        public decimal Kwota { get; set; }
+        public string Wlasciciel { get; set; }
+        public string Kategoria { get; set; }
+
+        public Budzet(int id, decimal kwota, string kategoria, string wlasciciel)
+        {
+            Id = id;
+            Kwota = kwota;
+            Kategoria = kategoria;
+            Wlasciciel = wlasciciel;
+
+        }
+
+    }
+}
